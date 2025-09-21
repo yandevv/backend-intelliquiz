67f13f4aac706b9e8d7900865c4df5cecddcf1f9
YanDevv - Macbook <ybritogomes@gmail.com>
2025-09-21 19:13:13 -0300

    feat(questions): added question schema

--- src/schemas/questionSchema.go
+++ src/schemas/questionSchema.go
@@ -8,13 +8,13 @@ import (
 )
 
 type Question struct {
-	ID        string `json:"id" gorm:"type:uuid;primaryKey"`
-	Content   string `json:"content" gorm:"not null"`
-	QuizID    string `json:"quiz_id" gorm:"not null"`
-	Quiz      Quiz
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	ID        string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	Content   string          `json:"content,omitempty" gorm:"not null"`
+	QuizID    string          `json:"quiz_id,omitempty" gorm:"not null"`
+	Quiz      *Quiz           `json:"quiz,omitempty"`
+	CreatedAt *time.Time      `json:"created_at,omitempty"`
+	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
+	DeletedAt *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 func (q *Question) BeforeCreate(tx *gorm.DB) (err error) {
