2fa402f2259e2413120b87511866813f1ed65a97
YanDevv - Macbook <ybritogomes@gmail.com>
2025-09-21 19:13:33 -0300

    feat(quizSchema): added created_by column user's relation on model

--- src/schemas/quizSchema.go
+++ src/schemas/quizSchema.go
@@ -12,6 +12,8 @@ type Quiz struct {
 	Name       string     `json:"name,omitempty" gorm:"not null"`
 	CategoryID string     `json:"category_id,omitempty" gorm:"not null"`
 	Category   *Category  `json:"category,omitempty"`
+	CreatedBy  string     `json:"created_by,omitempty"`
+	User       *User      `json:"user,omitempty" gorm:"foreignKey:CreatedBy"`
 	CreatedAt  *time.Time `json:"created_at,omitempty"`
 	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
 }
