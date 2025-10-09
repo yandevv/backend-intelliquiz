2719d42413eae0eba6432be008f1ebb9aa4163c3
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-09 11:33:52 -0300

    fix(quizScoreSchema): added optional on timestamps and omitempty on json marshalling

--- src/schemas/quizScoreSchema.go
+++ src/schemas/quizScoreSchema.go
@@ -13,10 +13,10 @@ type QuizScore struct {
 	Quiz      Quiz
 	UserID    string `json:"user_id" gorm:"not null"`
 	User      User
-	Score     uint           `json:"score" gorm:"not null"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	Score     uint            `json:"score" gorm:"not null"`
+	CreatedAt *time.Time      `json:"created_at,omitempty"`
+	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
+	DeletedAt *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 func (q *QuizScore) BeforeCreate(tx *gorm.DB) (err error) {
