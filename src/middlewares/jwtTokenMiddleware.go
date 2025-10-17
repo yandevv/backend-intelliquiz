3ad7c5092c2dfe369398d9eac3021e2f4819e40d
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-17 00:01:11 -0300

    feat(middlewares): added JWT token based middleware

---
+++ src/middlewares/jwtTokenMiddleware.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"intelliquiz/src/auth"
+	"intelliquiz/src/types"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func bearerFromHeader(c *gin.Context) string {
+	h := c.GetHeader("Authorization")
+	if after, ok := strings.CutPrefix(h, "Bearer "); ok {
+		return after
+	}
+
+	return ""
+}
+
+func JWTTokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenStr := bearerFromHeader(c)
+		if tokenStr == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, types.ForbiddenErrorResponseStruct{})
+			return
+		}
+
+		claims, err := auth.ParseAccess(tokenStr)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, types.ForbiddenErrorResponseStruct{})
+			return
+		}
+
+		c.Set("userID", claims.Subject)
+		c.Next()
+	}
+}
