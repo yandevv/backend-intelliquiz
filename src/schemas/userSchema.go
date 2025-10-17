ecd7b9a171879e9a17e9ab6afa93573a3fc65bd7
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-17 00:00:38 -0300

    feat(users): added username, password and email fields

--- src/schemas/userSchema.go
+++ src/schemas/userSchema.go
@@ -9,6 +9,9 @@ import (
 
 type User struct {
 	ID        string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	Username  string          `json:"username,omitempty" gorm:"unique;not null"`
+	Password  string          `json:"password,omitempty" gorm:"not null"`
+	Email     string          `json:"email,omitempty" gorm:"unique;not null"`
 	Name      string          `json:"name,omitempty" gorm:"not null"`
 	CreatedAt *time.Time      `json:"created_at,omitempty"`
 	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
