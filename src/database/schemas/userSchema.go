384cc2ee46367143428418d2eb68c1d083fe5904
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-19 10:43:15 -0300

    refactor(users): changed from unique to uniqueIndex username and email fields

--- src/database/schemas/userSchema.go
+++ src/database/schemas/userSchema.go
@@ -9,9 +9,9 @@ import (
 
 type User struct {
 	ID        string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
-	Username  string          `json:"username,omitempty" gorm:"size:50;unique;not null"`
+	Username  string          `json:"username,omitempty" gorm:"size:50;uniqueIndex;not null"`
 	Password  string          `json:"password,omitempty" gorm:"size:50;not null"`
-	Email     string          `json:"email,omitempty" gorm:"size:254;unique;not null"`
+	Email     string          `json:"email,omitempty" gorm:"size:254;uniqueIndex;not null"`
 	Name      string          `json:"name,omitempty" gorm:"size:60;not null"`
 	CreatedAt *time.Time      `json:"created_at,omitempty"`
 	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
