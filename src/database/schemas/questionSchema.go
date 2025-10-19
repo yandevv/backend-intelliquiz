d8a2e27922dcea328495f2f3302029a207612f3e
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-19 10:42:49 -0300

    feat(choices): added choice schema and added correct_choice_id on question schema

--- src/database/schemas/questionSchema.go
+++ src/database/schemas/questionSchema.go
@@ -8,13 +8,16 @@ import (
 )
 
 type Question struct {
-	ID        string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
-	Content   string          `json:"content,omitempty" gorm:"not null"`
-	QuizID    string          `json:"quiz_id,omitempty" gorm:"not null"`
-	Quiz      *Quiz           `json:"quiz,omitempty"`
-	CreatedAt *time.Time      `json:"created_at,omitempty"`
-	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
-	DeletedAt *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	ID              string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	Content         string          `json:"content,omitempty" gorm:"not null"`
+	QuizID          string          `json:"quiz_id,omitempty" gorm:"not null"`
+	Quiz            *Quiz           `json:"quiz,omitempty"`
+	CorrectChoiceID string          `json:"correct_choice_id,omitempty" gorm:"uniqueIndex;not null"`
+	CorrectChoice   *Choice         `json:"correct_choice,omitempty" gorm:"foreignKey:ID"`
+	Choices         []Choice        `json:"choices,omitempty" gorm:"foreignKey:QuestionID"`
+	CreatedAt       *time.Time      `json:"created_at,omitempty"`
+	UpdatedAt       *time.Time      `json:"updated_at,omitempty"`
+	DeletedAt       *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 func (q *Question) BeforeCreate(tx *gorm.DB) (err error) {
