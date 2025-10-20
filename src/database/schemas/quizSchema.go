a9aecb08a7c4d9f2fce182c38cb91d0c828cf56f
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-20 09:15:50 -0300

    refactor(quizSchema): added soft deletion

--- src/database/schemas/quizSchema.go
+++ src/database/schemas/quizSchema.go
@@ -8,15 +8,16 @@ import (
 )
 
 type Quiz struct {
-	ID         string     `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
-	Name       string     `json:"name,omitempty" gorm:"size:60;not null"`
-	CategoryID string     `json:"category_id,omitempty" gorm:"not null"`
-	Category   *Category  `json:"category,omitempty"`
-	CreatedBy  string     `json:"created_by,omitempty"`
-	User       *User      `json:"user,omitempty" gorm:"foreignKey:CreatedBy"`
-	Questions  []Question `json:"questions,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	CreatedAt  *time.Time `json:"created_at,omitempty"`
-	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
+	ID         string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	Name       string          `json:"name,omitempty" gorm:"size:60;not null"`
+	CategoryID string          `json:"category_id,omitempty" gorm:"not null"`
+	Category   *Category       `json:"category,omitempty"`
+	CreatedBy  string          `json:"created_by,omitempty"`
+	User       *User           `json:"user,omitempty" gorm:"foreignKey:CreatedBy"`
+	Questions  []Question      `json:"questions,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt  *time.Time      `json:"created_at,omitempty"`
+	UpdatedAt  *time.Time      `json:"updated_at,omitempty"`
+	DeletedAt  *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 func (q *Quiz) BeforeCreate(tx *gorm.DB) (err error) {
