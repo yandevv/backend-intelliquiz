b80fbbec2cb24392102daed0bbec2dec3e13ea38
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-25 12:39:25 -0300

    feat(gameQuestion): added answeredAt, removed choice_id not null constraint and added is_correct default false

--- src/database/schemas/gameQuestionSchema.go
+++ src/database/schemas/gameQuestionSchema.go
@@ -13,9 +13,10 @@ type GameQuestion struct {
 	Game       *Game      `json:"game,omitempty"`
 	QuestionID string     `json:"question_id,omitempty" gorm:"not null"`
 	Question   *Question  `json:"question,omitempty"`
-	ChoiceID   string     `json:"choice_id,omitempty" gorm:"not null"`
+	ChoiceID   *string    `json:"choice_id,omitempty"`
 	Choice     *Choice    `json:"choice,omitempty"`
-	IsCorrect  bool       `json:"is_correct" gorm:"not null"`
+	AnsweredAt *time.Time `json:"answered_at,omitempty"`
+	IsCorrect  bool       `json:"is_correct" gorm:"not null default:false"`
 	CreatedAt  *time.Time `json:"created_at,omitempty"`
 	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
 }
