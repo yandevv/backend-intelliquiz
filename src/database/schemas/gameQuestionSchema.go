a06a579803af156778bc74018bd915217f0d6fbe
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-25 19:46:23 -0300

    feat(gameQuestion): added SecondsTaken field on schema

--- src/database/schemas/gameQuestionSchema.go
+++ src/database/schemas/gameQuestionSchema.go
@@ -8,18 +8,19 @@ import (
 )
 
 type GameQuestion struct {
-	ID         string     `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
-	GameID     string     `json:"game_id,omitempty" gorm:"not null"`
-	Game       *Game      `json:"game,omitempty"`
-	QuestionID string     `json:"question_id,omitempty" gorm:"not null"`
-	Question   *Question  `json:"question,omitempty"`
-	ChoiceID   *string    `json:"choice_id,omitempty"`
-	Choice     *Choice    `json:"choice,omitempty"`
-	Position   uint8      `json:"position" gorm:"not null"`
-	AnsweredAt *time.Time `json:"answered_at,omitempty"`
-	IsCorrect  bool       `json:"is_correct" gorm:"not null default:false"`
-	CreatedAt  *time.Time `json:"created_at,omitempty"`
-	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
+	ID           string     `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	GameID       string     `json:"game_id,omitempty" gorm:"not null"`
+	Game         *Game      `json:"game,omitempty"`
+	QuestionID   string     `json:"question_id,omitempty" gorm:"not null"`
+	Question     *Question  `json:"question,omitempty"`
+	ChoiceID     *string    `json:"choice_id,omitempty"`
+	Choice       *Choice    `json:"choice,omitempty"`
+	Position     uint8      `json:"position" gorm:"not null"`
+	AnsweredAt   *time.Time `json:"answered_at,omitempty"`
+	SecondsTaken uint       `json:"seconds_taken" gorm:"-"`
+	IsCorrect    bool       `json:"is_correct" gorm:"not null default:false"`
+	CreatedAt    *time.Time `json:"created_at,omitempty"`
+	UpdatedAt    *time.Time `json:"updated_at,omitempty"`
 }
 
 func (q *GameQuestion) BeforeCreate(tx *gorm.DB) (err error) {
