72c950f09c9889d5150f874bced15293cd29a1e3
YanDevv - Macbook <ybritogomes@gmail.com>
2025-10-25 13:21:43 -0300

    feat(game): added is_finished and game question relation

--- src/database/schemas/gameSchema.go
+++ src/database/schemas/gameSchema.go
@@ -8,14 +8,16 @@ import (
 )
 
 type Game struct {
-	ID        string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
-	UserID    string          `json:"user_id,omitempty" gorm:"not null"`
-	User      *User           `json:"user,omitempty"`
-	QuizID    string          `json:"quiz_id,omitempty" gorm:"not null"`
-	Quiz      *Quiz           `json:"quiz,omitempty"`
-	CreatedAt *time.Time      `json:"created_at,omitempty"`
-	UpdatedAt *time.Time      `json:"updated_at,omitempty"`
-	DeletedAt *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	ID            string          `json:"id,omitempty" gorm:"type:uuid;primaryKey"`
+	UserID        string          `json:"user_id,omitempty" gorm:"not null"`
+	User          *User           `json:"user,omitempty"`
+	QuizID        string          `json:"quiz_id,omitempty" gorm:"not null"`
+	Quiz          *Quiz           `json:"quiz,omitempty"`
+	IsFinished    bool            `json:"is_finished,omitempty" gorm:"default:false"`
+	GameQuestions []GameQuestion  `json:"game_questions,omitempty"`
+	CreatedAt     *time.Time      `json:"created_at,omitempty"`
+	UpdatedAt     *time.Time      `json:"updated_at,omitempty"`
+	DeletedAt     *gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 func (g *Game) BeforeCreate(tx *gorm.DB) (err error) {
